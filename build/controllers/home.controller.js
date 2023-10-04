68af79e4c719dd95cda77e4c7ed736207e73489e
Mikołaj Marcińczak <39398823+mikolajmarcinczak@users.noreply.github.com>
2023-10-04 02:01:54 +0200

    Delete build directory

--- build/controllers/home.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class HomeController {
-    static welcome(req, res) {
-        return res.json({ message: "Welcome to reChord!" });
-    }
-}
-exports.default = HomeController;
--- build/utility/error.guard.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.assertIsError = void 0;
-function assertIsError(error) {
-    if (!(error instanceof Error)) {
-        throw error;
-    }
-}
-exports.assertIsError = assertIsError;
